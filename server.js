66e3555d089806e4c85a9b9f2d75e837512af9d5
Koustuve Shyamal <koustuve30@gmail.com>
2025-04-20 15:12:00 +0530

    Update codebase

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ const server = http.createServer((req,res) =>{
         ".js":"text/js",
         ".png":"image/png",
     }
-
+    
     const contentType = mimeTypes[extName]||"application/octet-stream"
 
     fs.readFile(filePath , (error , content) =>{
         if (error){
             if (error.code === "ENOENT"){
                 res.writeHead(400, {"content-type": "text/html"})
-                res.end("404 : file not found behen ke lode ")
+                res.end("404 : file not found")
             } else {
                 res.writeHead(500)
                 res.end(`server error ${error.code}`)
